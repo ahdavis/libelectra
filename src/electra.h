06d50abf4a5bb3108044af003e32d70b21c74e90
ahdavis <ahdavis13@gmail.com>
2020-01-07 14:20:28 -0600

    Added include file

---
+++ src/electra.h
@@ -0,0 +1,27 @@
+/*
+ * electra.h
+ * Master include file for libelectra
+ * Created on 1/7/2020
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2020  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//imports
+#import "args/ELCEventArgs.h"
+
+
+//end of header
